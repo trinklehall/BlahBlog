03fbdf46fdc7a3f05741c7910c07e5b90c5f355a
Gibran Parvez <gparvez@mail.umw.edu>
2012-11-19 14:05:00 -0500

    Added register, functional

--- Blahblog.java
+++ Blahblog.java
@@ -5,7 +5,24 @@ import java.util.Scanner;
 public class Blahblog{
 
         public static void main(String[] args) {
+		  String registerOrNo;
+
 		  Scanner input = new Scanner(System.in);
-	
+		  System.out.println("Welcome. Do you have an account? (Y/N)");
+		  registerOrNo = input.nextLine();
+		  if(registerOrNo.toLowerCase().equals("n")){
+
+				String username;
+				String password;  	
+		  		System.out.println("What username do you want?");
+				username = input.nextLine();
+				System.out.println("What password do you want?");
+				password = input.nextLine();
+				register Reg = new register();
+				Reg.newUser(username, password);
+				System.out.println("Great. Registered.");
+					
+		  }
+			
 		  }
 }
--- register.java
+++ register.java
@@ -5,6 +5,23 @@ import java.io.*;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.Writer;
+import java.io.FileNotFoundException;
 
-public class register(String user, String password){
+
+public class register{
+	public register(){
+	}
+	public void newUser(String username, String password){
+	try {
+    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("users.blah", true)));
+    out.println(username + " " + password);
+    out.close();
+} catch (IOException e) {
+    //oh noes!
+}
+
+	}
 }
